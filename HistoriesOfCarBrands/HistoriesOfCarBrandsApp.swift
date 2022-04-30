7e9427d9f729c4ec4b95141fbae256265265c7a3
Erislam Nurluyol <nurluyolerislam@gmail.com>
2022-04-30 12:01:30 +0300

    Initial Commit

---
+++ HistoriesOfCarBrands/HistoriesOfCarBrandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoriesOfCarBrandsApp.swift
+//  HistoriesOfCarBrands
+//
+//  Created by Erislam Nurluyol on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct HistoriesOfCarBrandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
